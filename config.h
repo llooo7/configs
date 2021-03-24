f698f8e9be59bc91d2e26382021bbc19c200397f
llooo7 <32995823+llooo7@users.noreply.github.com>
2021-03-25 00:17:43 +0100

    Update config.h

--- config.h
+++ config.h
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-    { Mode1Mask,                       KEY,      view,           {.ui = 1 << TAG} }, \
+    { Mod1Mask,                       KEY,      view,           {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
     { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
