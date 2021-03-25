4bbd0c85abbc4bbeebd67bfd34ac0c645546e7d8
llooo7 <32995823+llooo7@users.noreply.github.com>
2021-03-25 08:01:46 +0100

    Update config.h

--- config.h
+++ config.h
@@ -58,13 +58,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browser[]  = { "chromium", NULL };
+static const char *browser[]  = { "snap run brave", NULL };
+static const char *discord[]  = { "discord", NULL };
+static const char *spotify[]  = { "snap run spotify", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
     { Mod1Mask,                     XK_space,  spawn,         {.v = dmenucmd } },
     { MODKEY,                       XK_Return,      spawn,          {.v = termcmd } },
     { MODKEY,                       XK_2,      spawn,          {.v = browser } },
+    { MODKEY,                       XK_4,      spawn,          {.v = spotify } },
+    { MODKEY,                       XK_5,      spawn,          {.v = discord } },
     { MODKEY,                       XK_a,      togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
