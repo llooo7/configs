940688f8830f651775e9e1a6c602bc681e821a03
llooo7 <32995823+llooo7@users.noreply.github.com>
2021-03-27 16:38:42 +0100

    Update config.h

--- config.h
+++ config.h
@@ -13,11 +13,12 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_oran[]        = "#CA5010";
+static const char col_orli[]        = "fa6516";
 static const char col_ordk[]        = "#772E09";
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
-    [SchemeNorm] = { col_oran,  col_gray1, col_gray1 },
-    [SchemeSel]  = { col_gray1, col_oran,  col_oran  },
+    [SchemeNorm] = { col_orli,  col_gray1, col_gray1 },
+    [SchemeSel]  = { col_gray1, col_orli,  col_orli  },
 };
 
 
