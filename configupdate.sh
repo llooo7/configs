5931d8cccadc55aef0f6c64b433823dc8d821d99
llooo7 <32995823+llooo7@users.noreply.github.com>
2021-04-01 15:53:09 +0200

    Add files via upload

---
+++ configupdate.sh
@@ -0,0 +1,13 @@
+cd /home/luka/Downloads
+sudo rm -R configs
+sudo git clone https://github.com/llooo7/configs
+sudo rm -R dwm
+sudo git clone https://git.suckless.org/dwm
+cd dwm
+sudo make install
+sudo wget https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-20200508-7b77734.diff
+sudo patch < dwm-fullgaps-20200508-7b77734.diff
+cd /home/luka/Downloads/configs
+sudo cp config.h /home/luka/Downloads/dwm
+cd /home/luka/Downloads/dwm
+sudo make install
