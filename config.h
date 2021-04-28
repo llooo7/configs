8142d307491f01d0a23731c56689766207d42334
llooo7 <32995823+llooo7@users.noreply.github.com>
2021-04-28 23:12:44 +0200

    Update config.h

--- config.h
+++ config.h
@@ -76,7 +76,7 @@ static const char *browser[]  = { "brave", NULL };
 static const char *discord[]  = { "discord", NULL };
 static const char *spotify[]  = { "spotify", NULL };
 static const char *sublime[]  = { "sublime-text.subl", NULL };
-static const char *lockscr[]  = { "slock", NULL };
+static const char *lockscr[]  = { "slock & systemctl suspend", NULL };
 //static const char *screens[]  = { "import $(date + "%F-%H-%M").png", NULL }; 
 
 static Key keys[] = {
