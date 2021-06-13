63703f27ede61d5f977de4f1ee609439ba3c54f4
llooo7 <32995823+llooo7@users.noreply.github.com>
2021-06-14 00:52:43 +0200

    Update config.h

--- config.h
+++ config.h
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_walp, "-sb", col_walp, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *fileman[]  = { "st & nnn", NULL };
+static const char *fileman[]  = { "st & ranger", NULL };
 static const char *browser[]  = { "brave", NULL };
 static const char *discord[]  = { "discord", NULL };
 static const char *spotify[]  = { "spotify", NULL };
@@ -82,16 +82,16 @@ static const char *sublime[]  = { "sublime-text.subl", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-    { Mod1Mask,                     XK_space,  spawn,         {.v = dmenucmd } },
-    { MODKEY,                       XK_Return,      spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_1,      spawn,          {.v = fileman } },
-    { MODKEY,                       XK_2,      spawn,          {.v = browser } },
-    { MODKEY,                       XK_4,      spawn,          {.v = spotify } },
-    { MODKEY,                       XK_5,      spawn,          {.v = discord } },        
-    { MODKEY,                       XK_6,      spawn,          {.v = sublime } },
-    { Mod1Mask,                     XK_l,      spawn,          SHCMD("slock & systemctl suspend") },
+    { Mod1Mask,                     XK_space,  spawn,          	{.v = dmenucmd } },
+    { MODKEY,                       XK_Return, spawn,		{.v = termcmd } },
+    { MODKEY,                       XK_1,      spawn,          	{.v = fileman } },
+    { MODKEY,                       XK_2,      spawn,          	{.v = browser } },
+    { MODKEY,                       XK_4,      spawn,          	{.v = spotify } },
+    { MODKEY,                       XK_5,      spawn,          	{.v = discord } },        
+    { MODKEY,                       XK_6,      spawn,          	{.v = sublime } },
+    { Mod1Mask,                     XK_l,      spawn,          	SHCMD("slock & systemctl suspend") },
 //    { MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("import /home/luka/Screenshots/$(date +\"%F-%H-%M\").png" }, //screenshot
-    { MODKEY,                       XK_a,      togglebar,      {0} },
+    { MODKEY,                       XK_a,      togglebar,      	{0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
