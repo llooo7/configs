9ddcc71801bb389d6c2b2be085e85a5fa59d37b9
llooo7 <32995823+llooo7@users.noreply.github.com>
2022-05-21 11:24:02 +0200

    add awesomebar

--- configupdate.sh
+++ configupdate.sh
@@ -6,7 +6,9 @@ sudo git clone https://git.suckless.org/dwm
 cd dwm
 sudo make install
 sudo wget https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-20200508-7b77734.diff
+sudo wget https://dwm.suckless.org/patches/awesomebar/dwm-awesomebar-20200907-6.2.diff
 sudo patch < dwm-fullgaps-20200508-7b77734.diff
+sudo patch < dwm-awesomebar-20200907-6.2.diff
 cd /home/luka/Downloads/configs
 sudo cp config.h /home/luka/Downloads/dwm
 cd /home/luka/Downloads/dwm
