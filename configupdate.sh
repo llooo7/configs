a6efabca574e433c412e45f316272410a4de513d
llooo7 <32995823+llooo7@users.noreply.github.com>
2022-05-28 23:13:22 +0200

    Update configupdate.sh

--- configupdate.sh
+++ configupdate.sh
@@ -5,10 +5,8 @@ sudo rm -R dwm
 sudo git clone https://git.suckless.org/dwm
 cd dwm
 sudo make install
-sudo wget https://dwm.suckless.org/patches/awesomebar/dwm-awesomebar-20200907-6.2.diff
 sudo wget https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-20200508-7b77734.diff
 sudo patch < dwm-fullgaps-20200508-7b77734.diff
-sudo patch < dwm-awesomebar-20200907-6.2.diff
 cd /home/luka/Downloads/configs
 sudo cp config.h /home/luka/Downloads/dwm
 cd /home/luka/Downloads/dwm
